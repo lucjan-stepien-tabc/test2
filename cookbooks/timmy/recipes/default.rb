18fa03a34fc49a05375916313d61758f387732c4
lucjan-stepien-tabc <lucjan.stepien@ticket-text.com>
2015-12-18 14:50:26 +0000

    django recipe

--- cookbooks/timmy/recipes/default.rb
+++ cookbooks/timmy/recipes/default.rb
@@ -8,23 +8,4 @@ include_recipe "apt::default"
 include_recipe "apache2::default"
 include_recipe "apache2::mod_mpm_prefork"
 include_recipe "apache2::mod_wsgi"
-include_recipe "deploy"
 
-node[:deploy].each do |application, deploy|
-
-  if deploy[:application_type] != 'other'
-    Chef::Log.debug("Skipping deploy::django application #{application} as it is not a Django app")
-    next
-  end
-
-  opsworks_deploy_dir do
-    user deploy[:user]
-    group deploy[:group]
-    path deploy[:deploy_to]
-  end
-
-  opsworks_deploy do
-    deploy_data deploy
-    app application
-  end
-end
---
+++ cookbooks/timmy/recipes/django.rb
@@ -0,0 +1,20 @@
+include_recipe "deploy"
+
+node[:deploy].each do |application, deploy|
+
+  if deploy[:application_type] != 'other'
+    Chef::Log.debug("Skipping deploy::django application #{application} as it is not a Django app")
+    next
+  end
+
+  opsworks_deploy_dir do
+    user deploy[:user]
+    group deploy[:group]
+    path deploy[:deploy_to]
+  end
+
+  opsworks_deploy do
+    deploy_data deploy
+    app application
+  end
+end
