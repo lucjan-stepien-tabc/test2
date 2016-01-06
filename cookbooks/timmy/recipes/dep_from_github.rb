1c79cd0fd62708ee8aec40d6845e1757b3d0f54c
lucjan-stepien-tabc <lucjan.stepien@ticket-text.com>
2016-01-06 16:47:37 +0000

    back to old version

--- cookbooks/timmy/recipes/dep_from_github.rb
+++ cookbooks/timmy/recipes/dep_from_github.rb
@@ -6,19 +6,19 @@ execute "personel_git_token" do
   not_if "grep #{node[:deploy][:tix][:environment_variables][:git_personal_access_token]} #{node[:deploy][:tix][:deploy_to]}/current/tix/environment/dependencies_from_github.txt"
 end
 
-execute "install github dependencies" do
-  # environment(node[:deploy][:tix][:environment])
-  # user node[:opsworks][:deploy_user][:user]
-  command "#{node[:deploy][:tix][:deploy_to]}/current/virtualenv/bin/pip install --exists-action=w -r #{node[:deploy][:tix][:deploy_to]}/current/tix/environment/dependencies_from_github.txt"
-end
+# execute "install github dependencies" do
+#   environment(node[:deploy][:tix][:environment])
+#   user node[:opsworks][:deploy_user][:user]
+#   command "#{node[:deploy][:tix][:deploy_to]}/current/virtualenv/bin/pip install --exists-action=w -r #{node[:deploy][:tix][:deploy_to]}/current/tix/environment/dependencies_from_github.txt"
+# end
 
 #version before 
-# execute "install github dependencies" do
-#     user deploy[:user]
-#     environment(
-#       'PATH' => "#{node[:deploy][:tix][:deploy_to]}/current/virtualenv/bin:#{ENV['PATH']}", 
-#       'VIRTUAL_ENV ' => "#{node[:deploy][:tix][:deploy_to]}/current/virtualenv",
-#       'HOME' => "/home/#{deploy[:user]}"
-#       )
-#     command "#{node[:deploy][:tix][:deploy_to]}/current/virtualenv/bin/pip install --exists-action=w -r #{node[:deploy][:tix][:deploy_to]}/current/tix/environment/dependencies_from_github.txt"
-# end
+execute "install github dependencies" do
+    user deploy[:user]
+    environment(
+      'PATH' => "#{node[:deploy][:tix][:deploy_to]}/current/virtualenv/bin:#{ENV['PATH']}", 
+      'VIRTUAL_ENV ' => "#{node[:deploy][:tix][:deploy_to]}/current/virtualenv",
+      'HOME' => "/home/#{deploy[:user]}"
+      )
+    command "#{node[:deploy][:tix][:deploy_to]}/current/virtualenv/bin/pip install --exists-action=w -r #{node[:deploy][:tix][:deploy_to]}/current/tix/environment/dependencies_from_github.txt"
+end
